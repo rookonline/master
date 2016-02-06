31d378d800c08ae45e8111f368fbf57dabaa6200
Tom Watts <customer@rookonlinee.co.uk>
2016-02-06 19:59:55 +0000

    master directory created and pushed current live code

---
+++ default.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<title>Rook Online - Coming soon!</title>
+
+<font face="sans-serif">
+
+<body bgcolor="#feeddb">
+
+<center>
+
+<br>
+
+<br>
+
+<img src="images/rooklogo.jpg" alt="logo" height="220"width="400">
+
+<h2>Rook Online - Coming soon!</h2>
+</bt>
+</br>
+
+</a>
+
+<a href="http://www.instagram.com/rook_online">
+
+<img border="0" alt="instagram" src="images/instagram.png" width="60" height="60"></a>
+
+<a href="http://www.twitter.com/rook_online">
+
+<img border="0" alt="twitter" src="images/twitter.png" width="60" height="60"></a>
+</br></br></br>
+<footer>
+ <p>Rook LTD</p>
+  <p>Contact information: <a href="mailto:someone@example.com">
+  customer@rookltd.co.uk</a>.</p>
+</footer>
+
+</html>
